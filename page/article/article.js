a3d48a45957b3c63144fc6bfac4193177fca5c2c
kelrvins <k.elrvins@gmail.com>
2017-09-13 17:47:21 +0800

    文章页添加删除按钮

--- page/article/article.js
+++ page/article/article.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
     //设置title
     document.title = $("#articleTitle").text();
-    $("#articleDel").on("cilck", function (e) {
-        console.log(e);
-        // confirm("确认删除吗？")
+    $("#articleDel").on("click", function () {
+        var _id=$(this).data("id")
+        if(confirm("确认删除吗？")){
+            $({
+                type: "GET",
+                url: "",
+                data: _id,
+                success: function (data) {
+                    if (data[0] == 0) {
+                        alert("删除成功")
+                    } else {
+                        alert("删除失败")
+                    }
+                },
+                error: function (xhr, type) {
+                    alert("error")
+                }
+            })
+        }
     })
-    console.log($("#articleDel"))
 });
